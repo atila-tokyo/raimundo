23c1a53d0c20597fa70a6bac8c88753183006098
Antonio Victor Ribeiro Morelli <victor.morelli.007@gmail.com>
2021-03-01 15:42:52 -0300

    users seed implemented

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_01_173120) do
+ActiveRecord::Schema.define(version: 2021_03_01_180222) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "medicines", force: :cascade do |t|
+    t.float "dose"
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "reminders", force: :cascade do |t|
+    t.date "alarm_time"
+    t.string "content"
+    t.bigint "user_id", null: false
+    t.bigint "medicine_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["medicine_id"], name: "index_reminders_on_medicine_id"
+    t.index ["user_id"], name: "index_reminders_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -21,10 +39,15 @@ ActiveRecord::Schema.define(version: 2021_03_01_173120) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "username", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "reminders", "medicines"
+  add_foreign_key "reminders", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,73 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "Dropping Database"
+
+User.destroy_all
+Medicine.destroy_all
+Reminder.destroy_all
+
+user_first_names = [
+    "Victor", "Guilherme", "Atila", "William", "Rafael", "Roberto", "Admar",
+    "Fernando", "Mariana", "Matheus", "Beatriz", "Camila", "Gustavo", "Pedro",
+    "João", "Kayo", "Marcelo", "Ed", "Gabriel", "Felipe", "Leonardo",
+    "Lucas", "Yoda", "Anakim", "Rick", "Morty", "Han", "Luke",
+    "Chewbacca", "BB-8", "Boba", "R2-D2", "Jango", "Finn", "Kylo",
+    "Leia", "Jerry", "Frank", "Trevor", "Michael", "Lamar", "Tanisha",
+    "Kennedy", "Abraham", "Kennedy", "Houston", "Spike", "Hank", "Marie",
+    "Jesse", "Lucy", "Candance", "Ferb", "Kenny", "Eric", "Stan", "Buzz",
+    "Hermione", "Harry", "Marvin", "Severus", "Arthur"
+]
+
+user_last_names = [
+    "Skywalker", "Solo", "Fett", "Baba", "Nest", "Dameron", "Jinn",
+    "Ackbar", "Ren", "Emperor", "The Hut", "Maul", "Kenobi", "Smith",
+    "Sanchez", "Phillips", "Myers", "Ocean", "Lamar", "Spiegel", "Paul",
+    "Roiland", "McCormick", "Broflovski", "Cartman", "Marsh", "Lightyear",
+    "Chef", "Slave", "Granger", "Malfoy", "Snape", "Lee", "Wood",
+    "Williams", "Davis", "Mei", "Garcia", "Nguyen", "Carter", "Dumbledore",
+    "Snyde", "Snape", "Dent", "Perfect", "Beeblebrox", "Slartibartfast"
+]
+
+some_easy_passwords = "123123"
+
+email_endings = %w[
+                    @gmail.com @gmail.com.br @outlook.com @outlook.com.br @yahoo.com @yahoo.com.br 
+                    @hotmail.com @lewagon.com @lewagon.com.br @zoho.com @zoho.com.br @mail.com @mail.com.br
+                  ]
+
+
+# Setting emails trying to generate maximum uniques emails
+user_emails = []
+puts "Setting user emails"
+100.times do 
+    user_emails << "#{Faker::Ancient.god}_#{Faker::Space.constellation}#{email_endings.sample}".gsub(" ", "")
+    user_emails << "#{Faker::Ancient.primordial}_#{Faker::Space.constellation}#{email_endings.sample}".gsub(" ", "")
+    user_emails << "#{Faker::Ancient.titan}_#{Faker::Space.constellation}#{email_endings.sample}".gsub(" ", "")
+    user_emails << "#{Faker::Ancient.hero}_#{Faker::Space.constellation}#{email_endings.sample}".gsub(" ", "")
+    user_emails << "#{Faker::Artist.name}_#{Faker::Superhero.prefix}#{email_endings.sample}".gsub(" ", "")
+    user_emails << "#{Faker::Space.constellation}_#{Faker::Ancient.hero}#{email_endings.sample}".gsub(" ", "")
+    user_emails << "#{Faker::CryptoCoin.coin_name}_#{Faker::Ancient.hero}#{email_endings.sample}".gsub(" ", "")
+end
+
+user_emails.map!(&:downcase).uniq!
+
+# Creating users from seed
+puts "Creating Users"
+30.times do 
+    user_email = user_emails.sample
+    user_email = user_emails.sample until (User.pluck(:email).exclude?(user_email))
+
+    user = User.create!(
+        email: user_email,
+        password: some_easy_passwords,
+        first_name: user_first_names.sample,
+        last_name: user_last_names.sample,
+        username: "#{user_first_names.sample}_#{user_last_names.sample}".downcase
+    )
+
+    puts "\n#{user.id} - User Created - #{user.username}"
+    puts "_" * 55
+end
