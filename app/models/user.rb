307030ac4115152c60bbcef52bed01d65062370f
Atila Lima <atilamanager@gmail.com>
2021-03-02 03:22:07 +0900

    adding validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :reminders, dependent: :destroy
+  has_many :medicines, through: :reminders
+
+  validates :first_name, :last_name,
+            :username, presence: true
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
