1a3e7da05e6754f00f7d0baa9338444973481fbf
Antonio Victor Ribeiro Morelli <victor.morelli.007@gmail.com>
2021-03-03 22:06:05 -0300

    calendar working with some css, fixes, translated, added moment js to the project

--- app/javascript/components/date_flatpicker.js
+++ app/javascript/components/date_flatpicker.js
@@ -1,16 +1,27 @@
 import flatpickr from 'flatpickr';
+import 'flatpickr/dist/themes/airbnb.css';
+import 'flatpickr/dist/themes/material_orange.css';
+import { Portuguese } from 'flatpickr/dist/l10n/pt.js';
+import moment from './node_modules/moment/src/moment';
 
-
+// website https://flatpickr.js.org/options/
+// website http://t1m0n.name/air-datepicker/docs/#example-months
 
 const datepicker = () => {
-    flatpickr('.datepicker', {
-        
-    });
+	var currentdate = new Date();
+	if (flatpickr('.datepicker')) {
+		flatpickr('.datepicker', {
+			dateFormat: 'd-m-Y H:i',
+			locale: Portuguese,
+			altInput: true,
+			altFormat: 'F j, Y H:i',
+			enableTime: true,
+			defaultHour: currentdate.getHours(),
+			defaultMinute: currentdate.getMinutes(),
+			time_24hr: true,
+            minDate: currentdate
+		});
+	}
 };
-
-
-
-
-
 
 export { datepicker };
