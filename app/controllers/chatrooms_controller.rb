da08524e102f7ee977b3701824ef949ba7986659
Antonio Victor Ribeiro Morelli <victor.morelli.007@gmail.com>
2021-03-07 19:08:43 -0300

    better modeling

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -3,11 +3,30 @@ class ChatroomsController < ApplicationController
     @chatrooms = policy_scope(Chatroom).order(created_at: :desc)
   end
 
+  def new
+    @chatroom = Chatroom.new
+    authorize @chatroom
+  end
+
+  def create
+  end
+
   def show
     @chatrooms = Chatroom.all
     @chatroom = Chatroom.find(params[:id])
     @message = Message.new
     authorize @chatroom
     authorize @message
+  end
+
+  private
+
+  def chatroom_params
+    params.require(:chatroom).permit(:name, :user_recipient)
+  end
+
+  def set_chatroom
+    @chatroom = Message.find(params[:id])
+    authorize @chatroom
   end
 end
--- db/migrate/20210307191326_create_chatrooms.rb
+++ db/migrate/20210307191326_create_chatrooms.rb
@@ -2,7 +2,7 @@ class CreateChatrooms < ActiveRecord::Migration[6.1]
   def change
     create_table :chatrooms do |t|
       t.string :name
-      t.integer :user_recipient
+      t.string :user_recipient
       t.references :user, null: false, foreign_key: true
       t.timestamps
     end
