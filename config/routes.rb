4ccea09af0d9cd8114e50f58ceb4242f748a46bb
Antonio Victor Ribeiro Morelli <victor.morelli.007@gmail.com>
2021-03-07 16:45:34 -0300

    added resources messages only [create]

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :reminders
-  resources :chatrooms, only: %i[show]
+  resources :chatrooms, only: %i[show] do
+    resources :messages, only: %i[create]
+  end
 end
