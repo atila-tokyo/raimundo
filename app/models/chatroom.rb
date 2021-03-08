9d1d2f5006a3d0780565d5d3606d3f55a23c5bca
Antonio Victor Ribeiro Morelli <victor.morelli.007@gmail.com>
2021-03-08 14:19:03 -0300

    fixed model

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,6 +1,6 @@
 class Chatroom < ApplicationRecord
   belongs_to :user
   has_many :messages, dependent: :destroy
-  has_many :guests
+  has_many :guests, dependent: :destroy
   has_many :users, through: :guests
 end
