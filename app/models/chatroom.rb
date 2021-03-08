9d2d678ac919918c7d6ca7d08d4373e148a85045
Antonio Victor Ribeiro Morelli <victor.morelli.007@gmail.com>
2021-03-08 13:42:46 -0300

    model minor change

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,5 +1,5 @@
 class Chatroom < ApplicationRecord
   belongs_to :user
   has_many :messages, dependent: :destroy
-  has_many :users, through: :guests
+  has_many :users, through: :guests, optional: true
 end
