a49f8f95a909810b7bd4c12ea673af2c297a92d7
Antonio Victor Ribeiro Morelli <victor.morelli.007@gmail.com>
2021-03-10 16:13:09 -0300

    refactored code for guests controller

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -5,25 +5,44 @@ class GuestsController < ApplicationController
   end
 
   def destroy
-    # guest = Guest.where("user_id = ? AND chatroom_id = ?", params[:id], params[:format]).first
     if params[:format].present?
-      user = User.find(params[:id])
-      guest = Guest.find_by(user: user, chatroom: Chatroom.find(params[:format]))
+      destroy_one_guest(params)
       redirect_to chatroom_guests_path(params[:format])
-    elsif params[:chatroom][:users].present?
-      params[:chatroom][:users].each do |user_id|
-        user = User.find(user_id)
-        guest = Guest.find_by(user: user, chatroom: Chatroom.find(params[:id]))
-        guest.destroy
-      end
-        redirect_to chatroom_guests_path(params[:id])
-      
-
-    else
-      user = User.find(params[:id])
-      guest = Guest.find_by(user: user)
+    elsif params[:id].present? && !params[:chatroom].present?
+      guest_leave_chatroom(params)
       redirect_to chatrooms_path
+    elsif params[:chatroom][:users].present?
+      destroy_multiple_guests(params)
+      redirect_to chatroom_guests_path(params[:id])
+    else
+      return
     end
+  end
+
+  private
+
+  # inside chatroom/id/guests
+  def destroy_multiple_guests(params)
+    params[:chatroom][:users].each do |user_id|
+      user = User.find(user_id)
+      guest = Guest.find_by(user: user, chatroom: Chatroom.find(params[:id]))
+      authorize guest
+      guest.destroy
+    end
+  end
+
+  # destroy only one guest
+  def destroy_one_guest(params)
+    user = User.find(params[:id])
+    guest = Guest.find_by(user: user, chatroom: Chatroom.find(params[:format]))
+    authorize guest
+    guest.destroy
+  end
+
+  # destroy guest when user chooses to leave the chatroom
+  def guest_leave_chatroom(params)
+    user = User.find(params[:id])
+    guest = Guest.find_by(user: user)
     authorize guest
     guest.destroy
   end
